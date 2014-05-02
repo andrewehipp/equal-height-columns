75ec048fc478e0d94b7d840dfec6984a8ae5f56e
Andrew Hipp <andrewehipp@gmail.com>
2014-05-02 16:21:05 -0700

    Removed Legacy code and add minified version

--- _js/jquery.matchheight.js
+++ _js/jquery.matchheight.js
@@ -2,47 +2,49 @@
  
 	$.fn.matchHeight = function(options) {
 	
-        // Defualt Option Names.
+        // Defualt Options
 		var options = $.extend({
             children: "match",
             height: "min-height"
         }, options );
 
-	// Run for each instance
-	return this.each(function() {
+		// Run for each instance
+		return this.each(function() {
 
-		//Check if match height daya is set and if it's large than window size
-		if($(window).width() >= $(this).attr('data-match-height') || !$(this).attr('data-match-height')) {
+			//Parent Container
+			var container = $(this);
 
-			var match = $(this).find('.' + options.children); // Elements to apply Height to
-			var matchCount = match.length;
-			var matchHeight = match.outerHeight();
+			//Check if match height data is set and if it's large than window size
+			if($(window).width() >= container.attr('data-match-height') || !container.attr('data-match-height')) {
 
-			//Set largest Height out of all Child .match
-			$(match, $(this)).each(function(){
-				
-				//Test if current stored height is smaller, if so set new height
-				if(matchHeight < $($(this)).outerHeight()) {
-					matchHeight = $($(this)).outerHeight();						
-				}
+				var match = $(this).find('.' + options.children); // Elements to apply Height to
+				var matchHeight = 0;
 
-			});
+				//Set largest Height out of all Child .match
+				$(match, container).each(function(){
+					
+					//Test if current stored height is smaller, if so set new height
+					if(matchHeight < $($(this)).outerHeight()) {
+						matchHeight = $($(this)).outerHeight();						
+					}
 
-			$(match, $(this)).each(function(){
-				
-				//Test if current stored height is smaller, if so set new height
-				if($(this).attr('data-match-height-portion')) {
-					$(this).css(options.height, matchHeight * $(this).attr('data-match-height-portion'));
-				} else {
-					$(this).css(options.height, matchHeight);
-				}
+				});
 
-			});
+				// Apply Heights
+				$(match, container).each(function(){
+					
+					// If a data height portion exists use it
+					if($(this).attr('data-match-height-portion')) {
+						$(this).css(options.height, matchHeight * $(this).attr('data-match-height-portion'));
+					} else {
+						$(this).css(options.height, matchHeight);
+					}
 
-		}
+				});
 
-    });
+			}
 
+	    });
 
 	};
   
