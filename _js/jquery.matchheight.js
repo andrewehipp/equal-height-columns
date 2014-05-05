84756a5a44ac4364e50fcea009ebefce5d162e83
Andrew Hipp <andrewehipp@gmail.com>
2014-05-05 12:26:56 -0700

    Options and optimization

--- _js/jquery.matchheight.js
+++ _js/jquery.matchheight.js
@@ -5,39 +5,43 @@
         // Defualt Options
 		var options = $.extend({
             children: "match",
-            height: "min-height"
+            height: "min-height",
+            breakpoint: "768"
         }, options );
 
 		// Run for each instance
 		return this.each(function() {
 
 			//Parent Container
-			var container = $(this);
+			var $container = $(this);
+			var $winWidth = $(window).width();
 
 			//Check if match height data is set and if it's large than window size
-			if($(window).width() >= container.attr('data-match-height') || !container.attr('data-match-height')) {
+			if(($winWidth >= $container.attr('data-mh-breakpoint') || !$container.attr('data-mh-breakpoint')) && !($winWidth < options.breakpoint)) {
 
-				var match = $(this).find('.' + options.children); // Elements to apply Height to
-				var matchHeight = 0;
+				var $match = $(this).find('.' + options.children); // Elements to apply Height to
+				var $matchHeight = 0;
 
 				//Set largest Height out of all Child .match
-				$(match, container).each(function(){
+				$($match, $container).each(function(){
 					
+					var $newHeight = $(this).outerHeight();
+
 					//Test if current stored height is smaller, if so set new height
-					if(matchHeight < $($(this)).outerHeight()) {
-						matchHeight = $($(this)).outerHeight();						
+					if($matchHeight < $newHeight) {
+						$matchHeight = $newHeight;						
 					}
 
 				});
 
 				// Apply Heights
-				$(match, container).each(function(){
+				$($match, $container).each(function(){
 					
 					// If a data height portion exists use it
-					if($(this).attr('data-match-height-portion')) {
-						$(this).css(options.height, matchHeight * $(this).attr('data-match-height-portion'));
+					if($(this).attr('data-mh-scale')) {
+						$(this).css(options.height, $matchHeight * $(this).attr('data-mh-scale'));
 					} else {
-						$(this).css(options.height, matchHeight);
+						$(this).css(options.height, $matchHeight);
 					}
 
 				});
