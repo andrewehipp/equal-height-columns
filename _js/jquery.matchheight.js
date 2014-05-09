855631a0c8d51d08b770ea5c2b5c9fe261bb10ab
Andrew Hipp <andrewehipp@gmail.com>
2014-05-09 13:13:34 -0700

    Made plugin fully responsive
    Remeasures elements on the page when window is resized.
    No longer allows for height or min-height, just min-height.
    Removes min-height on elements when below the breakpoint.

--- _js/jquery.matchheight.js
+++ _js/jquery.matchheight.js
@@ -5,51 +5,71 @@
         // Defualt Options
 		var options = $.extend({
             children: "match",
-            height: "min-height",
             breakpoint: "768"
         }, options );
 
-		// Run for each instance
-		return this.each(function() {
+        var $matchHeight = $(this);
 
-			//Parent Container
-			var $container = $(this);
-			var $winWidth = $(window).width();
+		$(window).resize(function(){
 
-			//Check if match height data is set and if it's large than window size
-			if(($winWidth >= $container.attr('data-mh-breakpoint') || !$container.attr('data-mh-breakpoint')) && !($winWidth < options.breakpoint)) {
+			// Run for each instance
+			return $matchHeight.each(function() {
+
+				//Parent Container
+				var $container = $(this);
+				var $winWidth = $(window).width();
 
 				var $match = $(this).find('.' + options.children); // Elements to apply Height to
 				var $matchHeight = 0;
 
-				//Set largest Height out of all Child .match
-				$($match, $container).each(function(){
-					
-					var $newHeight = $(this).outerHeight();
+				//Check if match height data is set and if it's large than window size
+				if(($winWidth >= $container.attr('data-mh-breakpoint') || !$container.attr('data-mh-breakpoint')) && !($winWidth < options.breakpoint)) {
 
-					//Test if current stored height is smaller, if so set new height
-					if($matchHeight < $newHeight) {
-						$matchHeight = $newHeight;						
-					}
 
-				});
+					//Set largest Height out of all Child .match
+					$($match, $container).each(function(){
+						
+						// Store new height after clearing and checking new height
+						var $newHeight = $(this).css('min-height', 'initial').outerHeight();
 
-				// Apply Heights
-				$($match, $container).each(function(){
-					
-					// If a data height portion exists use it
-					if($(this).attr('data-mh-scale')) {
-						$(this).css(options.height, $matchHeight * $(this).attr('data-mh-scale'));
-					} else {
-						$(this).css(options.height, $matchHeight);
-					}
+						//Test if current stored height is smaller, if so set new height
+						if($matchHeight < $newHeight) {
+							$matchHeight = $newHeight;						
+						}
 
-				});
+					});
 
-			}
+					// Apply Heights
+					$($match, $container).each(function(){
+						
+						// If a data height portion exists use it
+						if($(this).attr('data-mh-scale')) {
+							$(this).css('min-height', $matchHeight * $(this).attr('data-mh-scale'));
+						} else {
+							$(this).css('min-height', $matchHeight);
+						}
 
-	    });
+					});
 
+				// If smaller than set and defualt breakpoints
+				} else {
+
+
+					//Loop through all .match
+					$($match, $container).each(function(){
+						
+						// Set no min-height
+						$(this).css('min-height', 'initial');
+
+
+					});
+
+
+				}
+
+			});
+
+		});
 	};
   
 }( jQuery ));
